7a8fd1de506e9bde2c59bcaa77406536441b339f
no1msd <no1msd@616fd16b-fc33-0410-b2f6-550dfd59928a>
2007-07-04 18:29:42 +0000

    Clean up in constants and direction

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -2,16 +2,8 @@
  * A programkodban hasznalt konstansok gyujto osztalya.
  */
 public class Constants {
-	//public enum CollectionKey { DESTROYABLE, SLOPE, DEAD, NOT_BLOCKING, DIRECTION }
-	//public enum Logic { TRUE, FALSE }
+	public enum CollectionKey { DESTROYABLE, SLOPE, DEAD, NOT_BLOCKING, DIRECTION }
+	public enum Logic { TRUE, FALSE }
 	
-	public static final String DESTROYABLE_KEY = "Destroyable";
-	public static final String SLOPE_KEY = "Slope";
-	public static final String DEAD_KEY = "Dead";
-	public static final String NOT_BLOCKING_KEY = "Dead";
-	public static final String DirectionKey = "Direction";
-	public static final String True = "true";
-	public static final String False = "false";
-
-	public static final int TILE_SIZE = 25;
+	public static final int TILE_SIZE = 50;
 }
